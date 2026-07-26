4b0d5c7345a811cf6648e796847babffb4228c00
ewan <eightysix86official@gmail.com>
2026-07-26 16:21:18 +0800

    Revert "Rebuild site as Next.js + TypeScript + Tailwind + GSAP (#1)"
    This reverts commit ad1b4283475b4dd8062566ef30a4efc278b7da49.

--- components/NoiseOverlay.tsx
+++
@@ -1,3 +0,0 @@
-export default function NoiseOverlay() {
-  return <div className="noise absolute inset-0" aria-hidden="true" />;
-}
--- content/menu.ts
+++
@@ -1,9 +0,0 @@
-export const localMenuItems = [
-  "Small Bites (Prawn Cracker & Doughnut)",
-  "Cupcorn",
-  "Houten Chilli Tapioca",
-  "Twisties",
-  "Hawflake",
-  "Ice Kacang",
-  "Rice & Egg",
-];
